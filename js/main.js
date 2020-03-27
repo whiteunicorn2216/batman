662b08a80f29a94aa286ea75a2a2d81c64611ef1
whiteunicorn2216 <62738574+whiteunicorn2216@users.noreply.github.com>
2020-03-27 11:45:31 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         let activeContent = $(this).attr('href');
         $('.visible').toggleClass('visible');
         $(activeContent).toggleClass('visible');
-        $('.tabs-item-active').toggleClass('')
+        $('.tabs-item-active').toggleClass('tabs-item-active')
         $(this).toggleClass('tabs-item-active')
     });
     
